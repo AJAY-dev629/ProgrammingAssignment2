a9137d33f55c1c7ba4a33c3fa137c7543e853258
AJAY-dev629 <mohitbhardwaj701500@gmail.com>
2024-11-23 00:20:02 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,5 +11,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
 }
